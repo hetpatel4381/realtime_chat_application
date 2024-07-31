7aa1627f6c5ba0506a02dd495be5db1c307ad444
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-31 14:10:45 +0530

    complete basic server setup with DB

--- chat_server/index.js
+++ chat_server/index.js
@@ -5,7 +5,19 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 
 connectDB();
+
 const app = express();
+
+app.use(
+  cors({
+    origin: [config.frontendOrigin],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.listen(config.port, () => {
   console.log(`Server running on  http://localhost:${config.port}`);
