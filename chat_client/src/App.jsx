ddc2777c3821d67498964a559f651c2852c29ca7
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-31 21:53:17 +0530

    setup get_user_info and loading in  client

--- chat_client/src/App.jsx
+++ chat_client/src/App.jsx
@@ -2,14 +2,76 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth";
 import Chat from "./pages/chat";
 import Profile from "./pages/profile";
+import { useAppStore } from "./store";
+import { useEffect, useState } from "react";
+import apiClient from "./lib/api-client";
+import { serverRoutes } from "./utils/constants";
+
+const PrivateRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to={"/auth"} />;
+};
+
+const AuthRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to={"/chat"} /> : children;
+};
 
 const App = () => {
+  const { userInfo, setUserInfo } = useAppStore();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserdata = async () => {
+      try {
+        const response = await apiClient.get(serverRoutes.GET_USER_INFO, {
+          withCredentials: true,
+        });
+        console.log("this is user response ", response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (userInfo) {
+      getUserdata();
+    } else {
+      setLoading(false);
+    }
+  }, [userInfo, setUserInfo]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<Navigate to={"/auth"} />} />
       </Routes>
     </BrowserRouter>
--- chat_client/src/utils/constants.js
+++ chat_client/src/utils/constants.js
@@ -4,6 +4,7 @@ const _serverRoutes = {
   AUTH_ROUTES,
   SIGNUP_ROUTE: `${AUTH_ROUTES}/signup`,
   LOGIN_ROUTE: `${AUTH_ROUTES}/login`,
+  GET_USER_INFO: `${AUTH_ROUTES}/userInfo`,
 };
 
 export const serverRoutes = Object.freeze(_serverRoutes);
