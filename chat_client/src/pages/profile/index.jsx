680b4c9c6c2d84e21cccd390deee40748ecd9760
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-08-01 14:48:33 +0530

    profile UI

--- chat_client/src/pages/profile/index.jsx
+++ chat_client/src/pages/profile/index.jsx
@@ -1,11 +1,125 @@
 import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarImage } from "@/components/ui/avatar";
+import { colors, getColor } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
-  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useAppStore();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
+
+  const saveChanges = () => {};
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
   return (
-    <div>
-      Profile
-      <div>Email: {userInfo.id}</div>
+    <div className="bg-[#1b1c24] h-[100vh] flex flex-col justify-center items-center gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90" />
+        </div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Avatar className="w-32 h-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`uppercase w-32 h-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName ? firstName.charAt(0) : userInfo.email.charAt(0)}
+                </div>
+              )}
+            </Avatar>
+            {hovered && (
+              <div className="absolute inset-0 flex justify-center items-center bg-black/50 rounded-full cursor-pointer">
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* <input type="text" /> */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+            <div className="w-full">
+              <input
+                type="email"
+                placeholder="Email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full flex gap-5">
+              {colors.map((color, index) => (
+                <div
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300 ${
+                    selectedColor === index
+                      ? "outline outline-white/50 outline-2"
+                      : ""
+                  }`}
+                  key={index}
+                  onClick={() => setSelectedColor(index)}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <Button
+            className="h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300"
+            onClick={saveChanges}
+          >
+            Save Changes
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
