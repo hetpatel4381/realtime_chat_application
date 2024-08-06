bc339b1bf3045dbcc32e53f9e21a098db81e9dff
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-08-06 06:19:17 +0530

    complete delete-profile-image controller

--- chat_server/controllers/userController.js
+++ chat_server/controllers/userController.js
@@ -166,6 +166,21 @@ const addProfileImage = async (req, res) => {
 
 const removeProfileImage = async (req, res) => {
   try {
+    const { userId } = req;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send("User not Found!");
+    }
+
+    if (user.image) {
+      unlinkSync(user.image);
+    }
+
+    user.image = null;
+    await user.save();
+
+    return res.status(200).send("Profile Image Removed Successfully!");
   } catch (error) {
     return res.status(500).send("Cannot delete profile image!");
   }
