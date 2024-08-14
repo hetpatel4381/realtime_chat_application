c575248495b5ca951503fc71103afb8e0741996e
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-08-14 18:23:11 +0530

    implement functionality in get-contacts-for-DM

--- chat_server/controllers/contactsController.js
+++ chat_server/controllers/contactsController.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import User from "../models/UserModel.js";
+import Message from "../models/MessagesModel.js";
 
 const searchContacts = async (req, res) => {
   try {
@@ -29,4 +31,63 @@ const searchContacts = async (req, res) => {
   }
 };
 
-export { searchContacts };
+const getContactsForDMList = async (req, res) => {
+  try {
+    let { userId } = req;
+    userId = new mongoose.Types.ObjectId(userId);
+
+    const contacts = await Message.aggregate([
+      {
+        $match: {
+          $or: [{ sender: userId }, { recipient: userId }],
+        },
+      },
+      {
+        $sort: { timestamp: -1 },
+      },
+      {
+        $group: {
+          _id: {
+            $cond: {
+              if: { $eq: ["sender", userId] },
+              then: "recipient",
+              else: "sender",
+            },
+          },
+          lastMessageTime: { $first: $timestamp },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "contactInfo",
+        },
+      },
+      {
+        $unwind: "contactInfo",
+      },
+      {
+        $project: {
+          _id: 1,
+          lastMessageTime: 1,
+          email: "$contactInfo.email",
+          firstName: "$contactInfo.firstName",
+          lastName: "$contactInfo.lastName",
+          image: "$contactInfo.image",
+          color: "$contactInfo.color",
+        },
+      },
+      {
+        $sort: { lastMessageTime: -1 },
+      },
+    ]);
+
+    return res.status(200).json({ contacts });
+  } catch (error) {
+    return res.status(500).send("Error while logging out the user!");
+  }
+};
+
+export { searchContacts, getContactsForDMList };
--- chat_server/routes/contactRoutes.js
+++ chat_server/routes/contactRoutes.js
@@ -5,5 +5,6 @@ import { searchContacts } from "../controllers/contactsController.js";
 const contactRouter = Router();
 
 contactRouter.post("/search", verifyJWT, searchContacts);
+contactRouter.get("/get-contacts-for-dm", verifyJWT, getContactsForDMList);
 
 export default contactRouter;
