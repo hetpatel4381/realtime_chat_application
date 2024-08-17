61d1a1cd9962559e1a4eab73e6a3231f2bff657c
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-08-17 05:57:41 +0530

    server side socket setup for send and receive channel message

--- chat_server/socket.js
+++ chat_server/socket.js
@@ -1,6 +1,7 @@
 import { Server as SocketIOServer } from "socket.io";
 import { config } from "./config/index.js";
 import Message from "./models/MessagesModel.js";
+import Channel from "./models/ChannelModel.js";
 
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
@@ -34,11 +35,50 @@ const setupSocket = (server) => {
       .populate("sender", "id email firstName lastName image color")
       .populate("recipient", "id email firstName lastName image color");
 
-    if(recipientSocketId){
+    if (recipientSocketId) {
       io.to(recipientSocketId).emit("recieveMessage", messageData);
     }
-    if(senderSocketId){
+    if (senderSocketId) {
       io.to(senderSocketId).emit("recieveMessage", messageData);
+    }
+  };
+
+  const sendChannelMessage = async (message) => {
+    const { channelId, sender, content, messageType, fileUrl } = message;
+
+    const createdMessage = await Message.create({
+      sender,
+      recipient: null,
+      content,
+      messageType,
+      timestamp: new Date(),
+      fileUrl,
+    });
+
+    const messageData = await Message.findById(createdMessage._id)
+      .populate("sender", "id email firstName lastName image color")
+      .exec();
+
+    await Channel.findByIdAndUpdate(channelId, {
+      $push: { message: createdMessage._id },
+    });
+
+    const channel = await Channel.findById(channelId).populate("members");
+
+    const finalData = { ...messageData._id, channelId: channel._id };
+
+    if (channel && channel.members) {
+      channel.members.forEach((member) => {
+        const memberSocketId = userSocketMap.get(member._id.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("receive-channel-message", finalData);
+        }
+
+        const adminSocketId = userSocketMap.get(channel.admin._id.toString());
+        if (adminSocketId) {
+          io.to(adminSocketId).emit("receive-channel-message", finalData);
+        }
+      });
     }
   };
 
@@ -52,6 +92,7 @@ const setupSocket = (server) => {
     }
 
     socket.on("sendMessage", sendMessage);
+    socket.on("send-channel-message", sendChannelMessage);
     socket.on("disconnect", () => disconnect(socket));
   });
 };
